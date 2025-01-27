af2ec0f9d48e2290ac670e40f93467e5055d4cea
kaiki nattan dos santos guedes <kaikinattansg@gmail.com>
2025-01-26 23:02:36 -0300

    Classe de seguranca

--- Test/Test/src/main/java/com/Test/Test/controller/MensagemController.java
+++ Test/Test/src/main/java/com/Test/Test/controller/MensagemController.java
@@ -3,7 +3,7 @@ package com.Test.Test.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //por se tratar de uma api rest
 public class MensagemController {
 
     @GetMapping("/")
---
+++ Test/Test/src/main/java/com/Test/Test/execoes/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.Test.Test.execoes;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
